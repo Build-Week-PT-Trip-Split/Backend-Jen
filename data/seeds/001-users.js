45de69a4ae31b34c7afc455a6e2f2570ee9813c5
sofialevin <slevin@M-4876.local>
2019-10-23 15:10:47 -0700

    Updated seed data.

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -8,90 +8,120 @@ exports.seed = function(knex, Promise) {
           username: "mario1",
           password: "pass",
           email: "test@gmail.com",
+          id: 0,
+          friends: [1, 2, 3]
         },
         {
           name: "Luigi",
           username: "luigi1",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 1,
+          friends: []
         },
         {
           name: "Bowser",
           username: "badguy",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 2,
+          friends: []
         },
         {
           name: "Peach",
           username: "princess",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 3,
+          friends: []
         },
         {
           name: "Joe",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 4,
+          friends: []
         },
         {
           name: "Matt",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 5,
+          friends: []
         },
         {
           name: "David",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 6,
+          friends: []
         },
         {
           name: "Jon",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 7,
+          friends: []
         },
         {
           name: "Ryan",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 8,
+          friends: []
         },
         {
           name: "Jen",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 9,
+          friends: []
         },
         {
           name: "Jack",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 10,
+          friends: []
         },
         {
           name: "Jill",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 11,
+          friends: []
         },
         {
           name: "Mike",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 12,
+          friends: []
         },
         {
           name: "Katie",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 13,
+          friends: []
         },
         {
           name: "Bill",
           username: "traveler",
           password: "pass",
-          email: "test@gmail.com"
+          email: "test@gmail.com",
+          id: 14,
+          friends: []
         }
 
 

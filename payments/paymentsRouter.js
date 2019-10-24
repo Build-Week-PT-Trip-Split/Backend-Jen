78e37c92e9e0458350605e7ed8165790ff1174ea
sofialevin <slevin@M-4876.local>
2019-10-23 21:08:29 -0700

    Updated routing for payments.

--- payments/paymentsRouter.js
+++ payments/paymentsRouter.js
@@ -1,13 +1,31 @@
 const router = require('express').Router();
 const Payments = require('./paymentsModel');
 
+const db = require('../data/dbConfig');
 
-router.get('/', async (req, res) => {
+router.get('/expense/:id', async (req, res) => {
   try {
-    const payments = await Payments.find();
+    const payments = await Payments.findBy({
+      expense_id: req.params.id
+    });
     res.status(200).json(payments);
   } catch (error) {
     res.status(500).json({ message: error + 'Uh-oh! There was an error retrieving your payments'})
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const payment = await Payments.findBy({ id: req.params.id });
+
+    if (payment) {
+      res.status(200).json(payment);
+    } else {
+      res.status(404).json({ message: "Payment not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
   }
 });
 
